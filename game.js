a8b27cd1c131d33c6a6f8349eace3b1d32f2d8a8
Tyler Mon <tylermoncrief88@yahoo.com>
2022-01-12 01:19:34 -0800

    add enemy combatants

--- game.js
+++ game.js
@@ -1,18 +1,12 @@
-// var playerName = 'Clank McKrank';
+var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyNames = ["Roborto", "AMy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + "index");
-}
 
 // fight function
 var fight = function(enemyName) {
